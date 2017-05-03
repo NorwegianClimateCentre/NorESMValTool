9d2c30e2e46d1f46077d4197bc6328b9c8b67115
ingo.bethke <ingo.bethke@uni.no>
2017-05-03 17:29:14 +0200

    Removed ESMValTool and noresm2cmor and changed install script to download these from github

--- install.sh
+++ install.sh
@@ -1,17 +1,40 @@
 #!/bin/sh -e
 
 echo "installing NorESMValTool folder in home directory"
+echo
 ESMVAL_REPO=`dirname \`readlink -f $0\`` 
 ESMVAL_HOME=$HOME/NorESMValTool 
 mkdir -p $ESMVAL_HOME
-cp -rp $ESMVAL_REPO/tools $ESMVAL_HOME
 cp -rp $ESMVAL_REPO/scripts $ESMVAL_HOME
 cp -rp $ESMVAL_REPO/mods $ESMVAL_HOME
-cp -rp update.sh $ESMVAL_HOME  
 cd $ESMVAL_HOME 
 ln -s $ESMVAL_REPO/data . 
 
+echo "donwload latest version of ESMValTool" 
+echo
+mkdir -p $ESMVAL_HOME/tools
+cd $ESMVAL_HOME/tools
+git clone https://github.com/ESMValGroup/ESMValTool.git
+
+echo "donwload and build latest version of noresm2cmor" 
+echo
+git clone https://github.com/NorwegianClimateCentre/noresm2cmor.git
+cd $ESMVAL_HOME/tools/noresm2cmor/build 
+if [ `uname -n | grep norstore | wc -l` -gt 0 ] 
+then 
+  . /usr/share/Modules/init/sh
+  module unload gcc netcdf netcdf.gnu hdf5.gnu 
+  module load gcc/4.7.2
+fi
+make -f Makefile.norstore_default
+mkdir -p $ESMVAL_HOME/tools/noresm2cmor/data
+cd $ESMVAL_HOME/tools/noresm2cmor/data 
+ln -sf /projects/NS2345K/noresm/cmorout cmorout
+ln -sf /projects/NS2345K/www/cmor/griddata griddata
+ln -sf /projects/NS2345K/www/cmor/sampledata sampledata
+
 echo "customize ESMValTool"
+echo
 ESMVAL_CONF=$ESMVAL_HOME/tools/ESMValTool/config_private.xml
 echo "  namelist with ESMValTool settings stored at ${ESMVAL_CONF}"
 ESMVAL_SCRATCH=/scratch/$USER/NorESMValTool
