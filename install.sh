e6ccf994b8dc936f1a2336ca56c5604c2a29f05d
ingo.bethke <ingo.bethke@uni.no>
2017-07-28 09:50:54 +0200

    Updated cmor-ization to work with latest version of noresm2cmor. Added modified ESMValTool namelists that are ported to NorStore.

--- install.sh
+++ install.sh
@@ -20,31 +20,30 @@ echo "donwload and build latest version of noresm2cmor"
 echo
 git clone https://github.com/NorwegianClimateCentre/noresm2cmor.git
 cd $ESMVAL_HOME/tools/noresm2cmor/build 
-if [ `uname -n | grep norstore | wc -l` -gt 0 ] 
-then 
-  . /usr/share/Modules/init/sh
-  module unload gcc netcdf netcdf.gnu hdf5.gnu 
-  module load gcc/4.7.2
-fi
-make -f Makefile.norstore_default
+. /usr/share/Modules/init/sh
+module unload gcc pgi netcdf hdf5 netcdf.gnu hdf5.gnu
+module load netcdf.intel/4.4.0 udunits/2.2.17 uuid/1.5.1  
+make -f Makefile_cmor2.norstore_intel
 mkdir -p $ESMVAL_HOME/tools/noresm2cmor/data
 cd $ESMVAL_HOME/tools/noresm2cmor/data 
-ln -sf /projects/NS2345K/noresm/cmorout cmorout
+ln -sf $ESMVAL_REPO/data/cmor cmorout
 ln -sf /projects/NS2345K/www/cmor/griddata griddata
 ln -sf /projects/NS2345K/www/cmor/sampledata sampledata
+cd $ESMVAL_HOME/scripts 
+ln -sf ../tools/noresm2cmor/scripts/cmorize_generic_intel.sh cmorize
 
 echo "customize ESMValTool"
 echo
 ESMVAL_CONF=$ESMVAL_HOME/tools/ESMValTool/config_private.xml
 echo "  namelist with ESMValTool settings stored at ${ESMVAL_CONF}"
-ESMVAL_SCRATCH=/scratch/$USER/NorESMValTool
 ESMVAL_PLOTPATH=$ESMVAL_HOME/plots
 ESMVAL_OBSPATH=$ESMVAL_HOME/data/obs
 ESMVAL_RAWOBSPATH=$ESMVAL_HOME/data/rawobs
 ESMVAL_MODELPATH=$ESMVAL_HOME/data/model
-ESMVAL_WORKPATH=$ESMVAL_SCRATCH/work
-ESMVAL_REGRPATH=$ESMVAL_SCRATCH/regrid
-ESMVAL_CLIMOPATH=$ESMVAL_SCRATCH/climo
+ESMVAL_WORKPATH=$ESMVAL_HOME/data/work
+ESMVAL_REGRPATH=$ESMVAL_HOME/data/regrid
+ESMVAL_CLIMOPATH=$ESMVAL_HOME/data/clim
+ESMVAL_CMORPATH=$ESMVAL_HOME/data/cmor
 sed -i "s%/path/to/work%${ESMVAL_WORKPATH}%" $ESMVAL_CONF
 echo "  ESMValTool work path set to ${ESMVAL_WORKPATH}"
 sed -i "s%/path/to/plots%${ESMVAL_PLOTPATH}%" $ESMVAL_CONF
@@ -61,5 +60,6 @@ sed -i "s%/path/to/data/obsdata%${ESMVAL_OBSPATH}%" $ESMVAL_CONF
 echo "  ESMValTool obs data path set to ${ESMVAL_OBSPATH}"
 sed -i "s%/path/to/data/rawobsdata%${ESMVAL_RAWOBSPATH}%" $ESMVAL_CONF
 echo "  ESMValTool rawobs data path set to ${ESMVAL_RAWOBSPATH}"
-mkdir -p $ESMVAL_HOME/plots $ESMVAL_SCRATCH/work $ESMVAL_SCRATCH/regrid $ESMVAL_SCRATCH/climo 
+echo "  ESMValTool cmor data path set to ${ESMVAL_CMORPATH}"
+mkdir -p $ESMVAL_HOME/plots
  
